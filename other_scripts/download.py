8b99d242ebb845e4859fbaf636cd19683414e289
s1910426 <a.c.rus@student.utwente.nl>
2020-03-19 19:14:46 +0100

    Cleaned main.py The rest of the code can be found in face_utils.py

--- other_scripts/download.py
+++ other_scripts/download.py
@@ -4,9 +4,7 @@ import os
 
 def download_file(filename, link):
     yt = YouTube(link)
-    if not os.path.isfile('./mp4_tracks/' + filename + '.mp4'):
-        yt.streams.filter(only_audio=True)[0].download(output_path='./mp4_tracks/', filename=filename)
+    if not os.path.isfile('\\mp4_tracks\\' + filename + '.mp4'):
+        yt.streams.filter(only_audio=True)[0].download(output_path='.\\mp4_tracks\\', filename=filename)
 
-    ffmpeg.input('./mp4_tracks/' + filename + '.mp4').output('../audio/tracks/' + filename + '.mp3').run()
-
-
+    ffmpeg.input('\\mp4_tracks\\' + filename + '.mp4').output('.\\audio\\tracks\\' + filename + '.mp3').run()
