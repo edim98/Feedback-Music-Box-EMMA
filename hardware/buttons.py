dbeee2d0ef6176657824c68e98642ddfeea9c4bb
Romme Knol <r.knol@student.utwente.nl>
2020-04-10 14:07:16 +0200

    Fixed missing variable reference

--- hardware/buttons.py
+++ hardware/buttons.py
@@ -1,76 +1,71 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import os
+import time
 import busio
 import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
+# Stop the system/music
 def stop(channel):
-    """ Called when the stop button is pressed.
-        Stops the system/music/pi(?)"""
-    print("Stop button was pushed!")
-    # TODO: stop whatever needs stopping
+    print("Stop Button was pushed!")
+    check_volume()
 
+# Skip the current song
 def skip(channel):
-    """ Called when the skip button is pressed.
-        Skips the current song."""
-    print("Skip button was pushed!")
-    # TODO: skip current song
+    print("Skip Button was pushed!")
+    set_volume(check_volume())
 
 # Remap a value from one range to another
 def remap_range(value, left_min, left_max, right_min, right_max):
-    """ Remaps a value from one range to another.
-       Used to convert raw ADC value to percentage."""
+    # this remaps a value from original (left) range to new (right) range
+    # Figure out how 'wide' each range is
     left_span = left_max - left_min
     right_span = right_max - right_min
  
+    # Convert the left range into a 0-1 range (int)
     valueScaled = int(value - left_min) / int(left_span)
-
+ 
+    # Convert the 0-1 range into a value in the right range.
     return int(right_min + (valueScaled * right_span))
 
+# Check the current desired volume, as read from the potentiometer
 def check_volume():
-    """ Reads the ADC to determine the volume the user has requested using the potentiometer.
-        Returns a percentage (0-100)"""
-    vol = remap_range(trim_pot, 0, 65535, 0, 100)
-    print('Requested Volume = {volume}%' .format(volume = vol))
-    return vol
+    # convert 16bit adc0 (0-65535) trim pot read into 0-100 volume level
+    trim_pot = chan0.value
+    set_volume = remap_range(trim_pot, 0, 65535, 0, 100)
+    print('Volume = {volume}%' .format(volume = set_volume))
+    return set_volume
 
-
+# Set the system volume to the given value
 def set_volume(vol):
-    """ Sets the audio volume of the operating system to the given value.
-        expects a value between 0 and 100."""
     set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' \
     .format(volume = vol)
     os.system(set_vol_cmd)
 
-def set_up():
-    """ Sets up the required pins and their response events.
-        Call before any other methods in this module.
-        ALWAYS CALL cleanup AFTER YOU'RE DONE!"""
-    stop_pin = 23 # BCM number of the input pin connected to the stop button
-    skip_pin = 24 # BCM number of the input pin connected to the skip button
-    
-    #GPIO.setwarnings(False) # Ignore warning for now
 
-    GPIO.setup(stop_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set stop_pin to be an input pin and set initial value to be pulled low (off)
-    GPIO.add_event_detect(stop_pin,GPIO.RISING,callback=stop) # Setup event on stop_pin rising edge
+print(GPIO.getmode())
+GPIO.setwarnings(False) # Ignore warning for now
+#GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
-    GPIO.setup(skip_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set skip_pin to be an input pin and set initial value to be pulled low (off)
-    GPIO.add_event_detect(skip_pin,GPIO.RISING,callback=skip) # Setup event on skip_pin rising edge
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(23,GPIO.RISING,callback=stop) # Setup event on pin 10 rising edge
 
-    # create the spi bus
-    spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-     
-    # create the cs (chip select)
-    cs = digitalio.DigitalInOut(board.D22)
-     
-    # create the mcp object
-    mcp = MCP.MCP3008(spi, cs)
-     
-    # create an analog input channel on pin 0
-    chan0 = AnalogIn(mcp, MCP.P0)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(24,GPIO.RISING,callback=skip) # Setup event on pin 10 rising edge
 
-def cleanup():
-    """ Performs the necessary cleanup."""
-    GPIO.cleanup()
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D22)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan0 = AnalogIn(mcp, MCP.P0)
+
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+GPIO.cleanup() # Clean up
